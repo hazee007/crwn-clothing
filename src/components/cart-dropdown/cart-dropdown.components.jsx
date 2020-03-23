c5f82eca5b5f86a705dbbfc002c6d66684c5e353
hazee007 <ajimoh2012@yahoo.com>
2020-03-23 15:41:35 +0400

    updating the app with reselect, replaced cart selectors with memorized selectors

--- src/components/cart-dropdown/cart-dropdown.components.jsx
+++ src/components/cart-dropdown/cart-dropdown.components.jsx
@@ -3,6 +3,7 @@ import CustomButton from '../custom-button/custom-button.component';
 import './cart-dropdown.style.scss';
 import { connect } from 'react-redux';
 import CartItem from '../cart-item/cart-item.components';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 
 
 const CartDropDown = ({cartItems}) => (
@@ -16,11 +17,15 @@ const CartDropDown = ({cartItems}) => (
         </div>
         <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
-)
+)  
 
 
-const mapStateToProps = ({cart: {cartItems}}) =>({
-    cartItems
+// const mapStateToProps = ({cart: {cartItems}}) =>({
+//     cartItems
+// });  this is replaced with the new code so that our application do not rerender(reload) when a state change in the app that does not affect this particular state /// this is what selectors are for..
+
+const mapStateToProps = state =>({
+    cartItems : selectCartItems(state)
 });
 
 
